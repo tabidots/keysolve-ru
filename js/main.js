e4a87b56b98bf06bd8c0fb550ce6cd99c8072878
reeve <150582459+grassfedreeve@users.noreply.github.com>
2025-03-26 12:14:41 +1100

    change github link

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ window.onload = async function() {
 }
 
 window.info = function() {
-    const url = 'https://github.com/ClemenPine/keysolve-web'
+    const url = 'https://github.com/grassfedreeve/keysolve-web'
     window.open(url, '_blank')
 }
 
